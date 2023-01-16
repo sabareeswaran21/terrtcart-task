ea21d05b8ca5494cbf7a15266c8c14a3cffdd62b
sabareeswaran21 <113236374+sabareeswaran21@users.noreply.github.com>
2023-01-16 14:13:21 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Addcatogery from './addcatogery';
 import Subcatogery from './subcatogery';
 
